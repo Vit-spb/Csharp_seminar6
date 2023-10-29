c77e94eb6fa48f6c7f2b224b8a5d114649e80cf8
Vit <wwg79@mail.ru>
2023-10-29 03:25:09 +0300

    ok

--- Tasks_002/Program.cs
+++ Tasks_002/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+string CheckTriangle(int b1, int k1, int b2, int k2)
+{
+if (a < b + c && b < a + c && c < a + b) return "Треугольник существует со сторонами такой длинны";
+return "Треугольник не существует со сторонами такой длинны";
+
+}
+
+Console.WriteLine("Введите значения b1, k1, b2 и k2: ");
+int[] myMrray = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+if (myMrray.Length!=4)
+{
+myMrray = (2,5,4,9)
+return;
+}
+
+Console.WriteLine(CheckTriangle(myMrray[0],myMrray[1],myMrray[2], myMrray[3]));
